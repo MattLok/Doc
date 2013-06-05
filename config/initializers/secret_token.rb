7b38b7932a5f339ae52ace36d70923a561435951
Matt Maloney <mwmaloney88@gmail.com>
2013-06-05 10:12:12 -0400

    initial commmit with db and gemfile prepared

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Docfer::Application.config.secret_token = '8fc25adaa5a4b742e116c438b03b6b2d6d79cfc0b7c2caed37e462051e4a0bac54f37c426e5bb4be8c51588f7a0d70ed5f4675e44ac1a3a9e277af91249dc155'
