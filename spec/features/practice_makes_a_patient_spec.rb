ea40b06ff3fb389fb94f107870a70a392093a549
Matt Maloney <mwmaloney88@gmail.com>
2013-06-25 18:24:18 -0400

    Patient test refactored to check for authorization

--- spec/features/practice_makes_a_patient_spec.rb
+++ spec/features/practice_makes_a_patient_spec.rb
@@ -6,25 +6,9 @@ feature "creating a patient" do
 
   it "practice creates a patient" do
     @prac = FactoryGirl.create(:prac_with_manager)
-    #@prac = Practice.create(:office_name => "CCMD", :email => "mmmm@mm.com", :contact_name => "Matt",:phone =>"5083621609")
-    visit(new_practice_patient_path(@prac))
-         
-    fill_in "First name", with:"Carl"
-    fill_in "Last name", with:"Banks"
-    fill_in "Ssn", with:"1234"
-    
-
-    page.status_code.should be 200
-    expect(page).to have_content("You made it")
-
-
-  end
-
-
-  it "checks if practice_membership has been formed on creation" do
-
-    count = PracticeMembership.count
-    @prac = Practice.create!(:office_name => "CCMD", :email => "mmmm@mm.com", :contact_name => "Matt",:phone =>"5083621609")
+    admin = @prac.users.first 
+    admin.password ='password'
+    sign_in_as(admin)
     visit(new_practice_patient_path(@prac))
          
     fill_in "First name", with:"Carl"
@@ -33,9 +17,28 @@ feature "creating a patient" do
 
     click_on "Create Patient"
 
+    page.status_code.should be 200
+    expect(page).to have_content("You made it")
+
+  end
+
+
+  it "checks if practice_membership has been formed on creation" do
+
+    count = PracticeMembership.count
+    @prac = FactoryGirl.create(:prac_with_manager)
+    admin = @prac.users.first 
+    admin.password ='password'
+    sign_in_as(admin)    
+    visit(new_practice_patient_path(@prac))
+         
+    fill_in "First name", with:"Carl"
+    fill_in "Last name", with:"Banks"
+    fill_in "Ssn", with:"1234"
+
+    click_on "Create Patient"
 
     expect(PracticeMembership.count).to eql((count+1))
-
 
   end
 
