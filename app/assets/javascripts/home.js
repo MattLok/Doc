5a9d412e494dda109a8dd80e67253e58c178bd85
Matt Maloney <mwmaloney88@gmail.com>
2013-07-02 15:22:40 -0400

    Basic rickshaw graph working, need to format json for referrals now

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,38 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+// $(document).ready(function(){
+
+
+//   $('li a').on("click", changeNavClass);
+//       // $('.side-nav li').removeClass('active');
+//       // $(this).parent().addClass('active');
+
+// });
+
+// function changeNavClass() {
+//   var pathname = window.location.pathname;
+
+//     alert(pathname);
+//     if(pathname.checkString("appointment"))
+//     // $('.side-nav li').removeClass('active');
+//     // //alert("AAAA");
+//     // $(this).parent().addClass('active');
+// }
+
+// function checkString(word){
+
+//   if (this.contains(word)){
+//     return true;
+//   }
+//   else
+//   {
+//     return false;
+//   }
+
+// }
+
+
+
+
